e17784766399df0bfde618b259b184d3b7ca0445
Sugih99 <sugihharto46@gmail.com>
2021-04-06 00:46:15 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () =>{
-    console.log(`Logged in as ${client.user.tag}!`);
+client.on('ready', () =>{
+    console.log('Logged in as ${client.user.tag}!');
     client.user.setPresence({
-        status: "online",  //You can show online, idle....
+        status: 'online',  //You can show online, idle....
         activity: {
-            name: "?meme",  //The message shown
-            type: "PLAYING" //PLAYING: WATCHING: LISTENING: STREAMING:
+            name: '?meme',  //The message shown
+            type: 'PLAYING' //PLAYING: WATCHING: LISTENING: STREAMING:
         }
     });
  });
