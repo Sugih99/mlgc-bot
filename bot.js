63ef6b49ace9ec0743768ec83d18fcce5b8307c6
Sugih99 <sugihharto46@gmail.com>
2021-04-05 22:37:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-        setInterval(() => {
-          targetGuild = client.guilds.get('GUILD ID HERE')
-          if(targetGuild) {
-              client.user.setPresence({ game: { name: targetGuild.memberCount + ' people in MLGC', type: 'WATCHING' }, status: 'online'  })
-                    .then(console.log)
-                    .catch(console.error);
-          }
-    }, 1000 * 60 * 5);
+    console.log('bot is online');
 
+    client.user.setPresence({
+        status: 'available',
+        activity: {
+            name: 'with ducks',
+            type: 'PLAYING',
+            url: 'https://discord.com'
+        }
+    });
 });
 
 client.on('message', message => {
