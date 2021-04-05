71a877f3f8263666ac9d3bfc405483457cee3311
Sugih99 <sugihharto46@gmail.com>
2021-04-05 22:47:21 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,21 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('bot is online');
+        setInterval(() => {
+          targetGuild = client.guilds.cache.get('294422759172472833')
+          if(targetGuild) {
+              client.user.setPresence({ game: { name: targetGuild.memberCount + ' people in this server, beware', type: 'WATCHING' }, status: 'online'  })
+                    .then(console.log)
+                    .catch(console.error);
+          }
+    }, 1000 * 60 * 5);
 
-    client.user.setPresence({
-        status: 'available',
-        activity: {
-            name: 'with ducks',
-            type: 'PLAYING',
-            url: 'https://discord.com'
-        }
-    });
 });
 
 client.on('message', message => {
     if (message.content === 'samlekom') {
-    	message.reply('komsalam anjaymabar');
+    	message.reply('komsalam');
   	}
 });
 
